612a5b1c6b6bb770df81b2091c4fdb050ea0918d
Kage Soichiro <k4gepedia@gmail.com>
2020-02-23 14:52:56 +0900

    sample data add

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module server
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,13 +1,54 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "ping update",
-		})
-	})
-	r.Run(":8888")
+	db, err := sql.Open("mysql", "user:password@tcp(portfolio_db_1:3306)/sample")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close() // 関数がリターンする直前に呼び出される
+
+	rows, err := db.Query("SELECT * FROM test_table") //
+	if err != nil {
+		panic(err.Error())
+	}
+
+	columns, err := rows.Columns() // カラム名を取得
+	if err != nil {
+		panic(err.Error())
+	}
+
+	values := make([]sql.RawBytes, len(columns))
+
+	//  rows.Scan は引数に `[]interface{}`が必要.
+
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var value string
+		for i, col := range values {
+			// Here we can check if the value is nil (NULL value)
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			fmt.Println(columns[i], ": ", value)
+		}
+		fmt.Println("-----------------------------------")
+	}
 }
